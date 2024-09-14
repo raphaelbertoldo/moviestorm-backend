5e7c2cf61a0470730ec07519a7f42e1c2f7176fa
Raphael Ferreira Bertoldo <rxphxel7k@gmail.com>
2024-09-14 17:47:29 -0300

    improve start server

--- src/startServer.js
+++ src/startServer.js
@@ -1,47 +1,22 @@
 import { Neo4jGraphQL } from "@neo4j/graphql";
 import dotenv from "dotenv";
 import { ApolloServer } from "apollo-server-express";
-import neo4j from "neo4j-driver";
 import express from "express";
 import { createServer } from "http";
 import { WebSocketServer } from "ws";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { useServer } from "graphql-ws/lib/use/ws";
 import cors from "cors";
+import { connectNeo4j } from "./providers/neo4j.js";
+import cron from "node-cron";
 
 dotenv.config();
 
-async function startServer({ typeDefs, resolvers }) {
-  const { NEO4J_URI, NEO4J_PASSWORD, PORT } = process.env;
+async function startServer() {
+  const schema = await connectNeo4j();
+  const { PORT } = process.env;
   const app = express();
-  var corsOptions = {
-    origin: "*",
-  };
   const httpServer = createServer(app);
-
-  const driver = neo4j.driver(
-    NEO4J_URI,
-    neo4j.auth.basic("neo4j", NEO4J_PASSWORD)
-  );
-  const neoSchema = new Neo4jGraphQL({
-    typeDefs,
-    driver,
-    plugins: {},
-  });
-
-  const wsServer = new WebSocketServer({
-    server: httpServer,
-    path: "/graphql",
-  });
-
-  const schema = await neoSchema.getSchema();
-  await neoSchema.checkNeo4jCompat();
-  const serverCleanup = useServer(
-    {
-      schema,
-    },
-    wsServer
-  );
 
   const server = new ApolloServer({
     schema,
@@ -56,19 +31,19 @@ async function startServer({ typeDefs, resolvers }) {
       ApolloServerPluginDrainHttpServer({
         httpServer,
       }),
-      {
-        async serverWillStart() {
-          return {
-            async drainServer() {
-              await serverCleanup.dispose();
-            },
-          };
-        },
-      },
     ],
   });
 
   await server.start();
+  app.use(
+    cors({
+      origin: ["*", "http://localhost:8080"],
+      credentials: true,
+    })
+  );
+  var corsOptions = {
+    origin: "*",
+  };
   app.options("*", cors(corsOptions));
 
   server.applyMiddleware({
@@ -78,24 +53,7 @@ async function startServer({ typeDefs, resolvers }) {
   });
 
   httpServer.listen(PORT, () => {
-    console.log(`Server is now running on http://localhost:${PORT}/graphql`);
-  });
-
-  const initCypher = `CALL apoc.schema.assert({}, {})`;
-
-  const executeQuery = (driver) => {
-    const session = driver.session();
-    return session
-      .writeTransaction((tx) => tx.run(initCypher))
-      .then(console.log("Neo4j iniciado com sucesso !"))
-      .finally(() => session.close());
-  };
-
-  executeQuery(driver).catch((error) => {
-    console.error(
-      "Database initialization failed to complete\n",
-      error.message
-    );
+    console.log(`Server is now running 🚅 on port ${PORT}`);
   });
 }
 
